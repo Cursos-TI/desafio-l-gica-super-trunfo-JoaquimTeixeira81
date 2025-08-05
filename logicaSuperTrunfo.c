78bbe9b493786591a960ab3f1b8eb2377c9b242a
JoaquimTeixeira81 <joaquimnonato20@gmail.com>
2025-08-05 01:18:51 +0000

    Versão em Atualização 1.6

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -141,27 +141,28 @@
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: População\n");
-     } else {
+     } 
+     else if (população1 < população2)
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: População\n");
-     }
-     if (população1 == população2) 
-     {
+     } 
+     else {
       printf("Empate: Carta 1 e Carta 2\n");
-      printf("Atributo empatado: População\n");
+      printf("Atributo empatado: Área\n");
      }
-
+  
      if (area1 > area2)
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: Área\n");
-     } else {
+     } 
+     else if (area1 < area2) 
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: Área\n");
      }
-
-     if (area1 == area2) 
-     {
+     else {
       printf("Empate: Carta 1 e Carta 2\n");
       printf("Atributo empatado: Área\n");
      }
@@ -170,11 +171,13 @@
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: PIB\n");
-     } else {
+     } 
+     else if (pib1 < pib2)
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: PIB\n");
      }
-     if (pib1 == pib2) 
+     else
      {
       printf("Empate: Carta 1 e Carta 2\n");
       printf("Atributo empatado: PIB\n");
@@ -184,20 +187,29 @@
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: Pontos Turisticos\n");
-     } else {
+     } 
+     else if(pontos1 < pontos2) 
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: Ponto Turisticos\n");
+     }
+     else
+     {
+      printf("Empate: Carta 1 e Carta 2\n");
+      printf("Atributo empatado: Pontos Turisticos\n");
      }
 
      if (densidade1 < densidade2)
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: Densidade Populacional\n");
-     } else {
+     } 
+     else if (densidade1 > densidade2) 
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: Densidade Populacional\n");
      }
-     if (densidade1 == densidade2) 
+     else
      {
       printf("Empate: Carta 1 e Carta 2\n");
       printf("Atributo empatado: Densidade Populacional\n");
@@ -207,27 +219,28 @@
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: Renda Per Capita\n");
-     } else {
+     } 
+     else if (percapita1 < percapita2) 
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: Renda Per Capita\n");
      } 
-     
-     if (percapita1 == percapita2) 
+     else 
      {
       printf("Empate: Carta 1 e Carta 2\n");
       printf("Atributo empatado: Renda Per Capita\n");
      }
-
-
      if (superpoder1 > superpoder2)
      {
       printf("Carta 1: São Paulo Venceu!\n");
       printf("Atributo Vencedor: Super Poder\n");
-     } else {
+     } 
+     else if(superpoder1 > superpoder2) 
+     {
       printf("Carta 2: Rio de Janeiro Venceu!\n");
       printf("Atributo Vencedor: Super Poder\n");
      }
-     if (superpoder1 == superpoder2) 
+     else 
      {
       printf("Empate: Carta 1 e Carta 2\n");
       printf("Atributo empatado: Super Poder\n");
